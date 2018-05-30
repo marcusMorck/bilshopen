9b23a50b63d0fb66f26c6e2c017091c6cfe8cefd
marcusMorck <marcus.morck@gmail.com>
2018-05-30 09:43:05 +0200

    added acl and more

--- client/client.js
+++ client/client.js
@@ -3,12 +3,21 @@
 Vue.component('welcome', WelcomeComponent);
 Vue.component('nav-menu', MenuComponent);
 
-const http = axios; // using axios 3rd part XHR/REST lib
+//const http = axios; // using axios 3rd part XHR/REST lib
 
-
+// Configure the router:
+// about the VueRouter: https://www.liquidlight.co.uk/blog/article/building-a-vue-v2-js-app-using-vue-router/
+const router = new VueRouter({
+  mode: 'history', // html5 popstate, alternatively: 'hash'
+  base: '/', // set the correct base
+  routes: [ // our frontend routes
+    //{ path: '/', component: HomePageComponent },
+    //{ path: '/products', component: ProductPageComponent }
+  ]
+});
 
 // the app
 let app = new Vue({
     el: "#app",
-    
+    router
   });
--- server/server.js
+++ server/server.js
@@ -3,12 +3,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 global.mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/bilshoppen');
 app.use(bodyParser.json());
 
 // serve frontend files
 app.use(express.static('../client/'));
+
+
+const AccessManager = require('access-manager');
+const accessManager = new AccessManager({
+  mongoose: mongoose, // mongoose (connected)
+  expressApp: app // an express app
+});
+
+
+// Models
+const User = accessManager.models.user;
+//Catch get request and send them to index.html, except /rest
+app.get(/^((?!rest).)*$/, async(req, res)=>{
+  res.sendFile(path.normalize(__dirname + '/../client/index.html'));
+});
+
 // start the express HTTP server
 app.listen('3000', ()=>{
     console.log('bilshoppen server is running on port 3000');
